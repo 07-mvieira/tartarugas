192b76e05ced3cc15961dd9daa89f00372877700
07-mvieira <caio.vieira2807@gmail.com>
2024-09-06 09:58:26 -0300

    q
    a

---
+++ script.js
@@ -0,0 +1,9 @@
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+  //if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    //document.getElementsByName("header").style.height = "1rem";
+  //} else {
+    //document.getElementsByName("header").style.height = "7rem";
+  //}
+//}
